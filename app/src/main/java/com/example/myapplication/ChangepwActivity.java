64d6950664d0b55b25444dbceaa1485c6306c52d
jiyuuuun <intesys1004@naver.com>
2023-08-06 17:02:54 +0900

    비밀번호 재설정

---
+++ app/src/main/java/com/example/myapplication/ChangepwActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ChangepwActivity extends AppCompatActivity {
+
+    Button btn;
+    EditText em;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_setting);
+
+        em = findViewById(R.id.email);
+        btn = findViewById(R.id.send);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String e=em.getText().toString();
+                FirebaseAuth.getInstance().sendPasswordResetEmail(e).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(ChangepwActivity.this, "send mail", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(ChangepwActivity.this, "Fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/myapplication/SettingFragment.java
+++ app/src/main/java/com/example/myapplication/SettingFragment.java
@@ -1,17 +1,23 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingFragment extends Fragment {
@@ -20,7 +26,8 @@ public class SettingFragment extends Fragment {
     private Switch mode_switch;
     private TextView modeStatus;
     private View view;
-
+    private Button btn_send;
+    private EditText em;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -41,6 +48,26 @@ public class SettingFragment extends Fragment {
             Intent intent = new Intent(getActivity(), LoginActivity.class);
             startActivity(intent);
         });
+
+        //비밀번호 재설정
+        em = view.findViewById(R.id.email);
+        btn_send = view.findViewById(R.id.send);
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String e = em.getText().toString();
+                FirebaseAuth.getInstance().sendPasswordResetEmail(e).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getActivity(), "send mail", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getActivity(), "Fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
         //다크모드
         mode_switch = view.findViewById(R.id.mode_switch);
         modeStatus = view.findViewById(R.id.tv_mode);
@@ -49,7 +76,7 @@ public class SettingFragment extends Fragment {
             modeStatus.setText("Dark Mode");
         } else if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_UNSPECIFIED){
             modeStatus.setText("UnSpecified Mode");
-        }else {
+        } else {
             modeStatus.setText("Light Mode");
         }
 
